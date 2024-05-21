762d3abe13b1e19121f6391862641c18276c79b1
Juan-Royes <40674146+Juan-Royes@users.noreply.github.com>
2024-05-20 23:42:46 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -102,7 +102,7 @@ export default {
       "pilots": [
         {
           "callsign": "Dancing Queen",
-          "Name": "Vittoria Bulksgurad",
+          "alias": "Vittoria Bulksguard",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-STELLARDYNAMICS-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "GMS",
           "frame": "Chomolugna",
